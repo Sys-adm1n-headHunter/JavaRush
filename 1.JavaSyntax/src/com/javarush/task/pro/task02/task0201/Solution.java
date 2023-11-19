d20ad8301de1f17edf59242a61d3ee813330d399
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-11-20 02:00:28 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task02.task0201;
+
+/*
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        int intValue = 1;
+         int numericValue=3;
+          int size =4;
+           int number=2;
+
+
+    }
+}
