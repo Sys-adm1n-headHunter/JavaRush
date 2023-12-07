86a2deebad10723344e700d5d5f0f459261c25ee
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-08 02:20:02 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0415/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0415;
+
+import java.util.Scanner;
+
+/* 
+Площадь круга
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        int S = (int)(3.14*radius*radius);
+        System.out.println(S);
+    }
+}
