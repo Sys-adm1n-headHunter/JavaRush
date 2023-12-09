e1542d8beee980f9dea3b08cf4db3ff26f366a98
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-09 20:56:35 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task06.task0608;
+
+/* 
+Кубический калькулятор
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        long a = 3;
+        System.out.println(cube(a));
+
+    }
+
+    //напишите тут ваш код
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+}
