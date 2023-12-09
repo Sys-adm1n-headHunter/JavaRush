72ab69f8a0c6b697684d2aafa13f9437d6131ce6
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-09 20:26:03 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+    //напишите тут ваш код
+
+    public static void universalMethod(int n[]) {
+
+    }
+
+    public static void universalMethod(int n[][]) {
+
+    }
+
+    public static void universalMethod(int n[][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][][][][][]) {
+
+    }
+
+    public static void universalMethod(int n[][][][][][][][][][]) {
+
+    }
+
+
+}
