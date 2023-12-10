029834b38ff22f93c3636dfbb95302511c94e053
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-10 03:17:17 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte b = 1;
+    short s = 1000;
+    int i = 100000;
+    long l = 1000000000000L;
+
+    float f = 0.0001f;
+    double d = 0.00000000123;
+
+    boolean bl = true;
+
+    char c = 'c';
+}
