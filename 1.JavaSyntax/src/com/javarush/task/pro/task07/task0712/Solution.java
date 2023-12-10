1c628f76a9336f04e0b3e6d5afdf74b36ce3a435
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-10 04:08:01 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String s1 = new String("1");
+        String s2 = new String("2");
+        String s3 = new String("3");
+        String s4 = new String("4");
+        String s5 = new String("5");
+        String s6 = new String("6");
+        String s7 = new String("7");
+        String s8 = new String("8");
+        String s9 = new String("9");
+        String s10 = new String("10");
+
+        int[] data1 = new int[10];
+        int[] data2 = new int[10];
+        int[] data3 = new int[10];
+        int[] data4 = new int[10];
+        int[] data5 = new int[10];
+
+        Scanner console1 = new Scanner("");
+        Scanner console2 = new Scanner("");
+
+    }
+}
