c567270779d007b522fd753a7906282d131707bf
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-21 07:00:11 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0914/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task09.task0914;
+
+/* 
+Обновление пути
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin/";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        //напишите тут ваш код
+        int firsIndex = path.indexOf("jdk");
+        int lasIndex = path.indexOf("/",firsIndex);
+        String chandgePath = path.substring(0,firsIndex) + jdk + path.substring(lasIndex);
+        return chandgePath;
+    }
+}
