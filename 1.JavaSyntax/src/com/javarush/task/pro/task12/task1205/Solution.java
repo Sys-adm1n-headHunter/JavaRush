bd8208b5d4d3614608f6966a84d8cb4e0eafec34
Sys-adm1n-headHunter <greatest.sa.hh.2022@gmail.com>
2023-12-23 00:02:50 +0300

    Решение задач

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1205/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task12.task1205;
+
+/* 
+Метод деления
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divide(1, 0);
+        divide(-1, 0);
+        divide(0, 0);
+        divide(100, 20);
+        divide(200, 20);
+    }
+
+    public static void divide(double a, double b) {
+        //напишите тут ваш код
+        Double aBox = a;
+        Double bBox =b;
+        Double cBox = a/b;
+
+        System.out.println(cBox);
+    }
+}
